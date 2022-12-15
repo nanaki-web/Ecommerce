3b9b54144a294c25e14e9bca476f11ee94d8371b
Anne Wilga <anne.wilga@orange.fr>
2022-12-15 13:02:08 +0100

    +Créer le fichier xml +Lien entre xml et java

--- app/src/main/java/com/example/ecommerce/MainActivity.java
+++ app/src/main/java/com/example/ecommerce/MainActivity.java
@@ -3,12 +3,44 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView errorConnectAccountTextView;
+    private EditText usernameEditText;
+    private EditText passwordEditText;
+
+    private Button connectBtn;
+    private TextView createAccountBtn;
+
+    private String username;
+    private String password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        errorConnectAccountTextView = findViewById(R.id.errorConnectAccountTextView);
+        usernameEditText = findViewById(R.id.usernameEditText);
+        passwordEditText = findViewById(R.id.passwordEditText);
+        connectBtn = findViewById(R.id.connectBtn);
+        createAccountBtn = findViewById(R.id.createAccountBtn);
+
+        //pour récupérer les valeurs utilisateur et mot de passe
+        connectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                username = usernameEditText.getText().toString();
+                password = passwordEditText.getText().toString();
+
+                //lancer la requête pour connecter l'utilisateur
+
+            }
+        });
     }
 }
