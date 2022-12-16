c460875918ad079ab8ed720f7860f113ac62fd8c
Anne Wilga <anne.wilga@orange.fr>
2022-12-16 12:21:56 +0100

    +affichage nom d'utilisateur sur activity_interface.xml

--- app/src/main/java/com/example/ecommerce/InterfaceActivity.java
+++ app/src/main/java/com/example/ecommerce/InterfaceActivity.java
@@ -3,12 +3,22 @@ package com.example.ecommerce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class InterfaceActivity extends AppCompatActivity {
+
+    private TextView welcomeTextView;
+    private String username;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_interface);
+
+        welcomeTextView = findViewById(R.id.welcomeTextView);
+        //recuperer l'username
+        username = getIntent().getStringExtra("username");
+
+        welcomeTextView.setText("bienvenu : " +username);
     }
 }
